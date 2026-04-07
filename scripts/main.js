705413aa5b60120847ce37dc9d147ec134e7fc26
Eggspert7820 <gabrieldarosa7820@gmail.com>
2026-04-07 19:49:54 -0400

    Updated css, added new functionality to the generate button

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,27 @@
 function main(){
-    var newSection = document.getElementById('subway-times')
-    var innerHTML = ""
-    for(var x = 1; x<=10; x++) {
-        innerHTML += `<h2>Section Title ${x}</h2><p>This section was created dynamically using JavaScript.</p>`;
-    }
-    newSection.innerHTML=innerHTML
+    var generate = document.getElementById("generate")
+    generate.addEventListener("mouseup", generateTrains)
 }
 
+function generateTrains() {
+    var stationsofinterest = document.getElementById("stationsofinterest")
+    var crossstreets = document.getElementById("crossstreets")
+    var address = document.getElementById("address")
+    var trainInfo = ""
+    if (stationsofinterest.value) {
+        trainInfo += `<p>Interested stations: ${stationsofinterest.value}</p>`
+    }
+    if (crossstreets.value) {
+        trainInfo += `<p>Interested cross streets: ${crossstreets.value}</p>`
+    }
+    if (address.value) {
+        trainInfo += `<p>Address: ${address.value}</p>`
+    }
+
+    var subwayTimes = document.getElementById('subway-times')
+    genSecCount++
+    subwayTimes.innerHTML += `<h2>Subway Section Title ${genSecCount}</h2>${trainInfo}`
+}
+
+var genSecCount=0
 main()
